8997133a51cc62694c4ab4e041c35e34452db192
sasikumarsibbala <65108079+sasikumarsibbala@users.noreply.github.com>
2021-07-16 17:17:33 +0530

    Update gradeSystem.js

--- src/gradeSystem.js
+++ src/gradeSystem.js
@@ -8,17 +8,25 @@ class gradeSystem{
         this.failed=JsonJob.failed;
         this.skipped=JsonJob.skipped;
         this.aborted=JsonJob.aborted;
-
-
-
+    
     }
+
     calculateBuildGrade (){
         let grade="";
-        if(this.total<=0|| this.passed<=0||this.failed<0||this.skipped<0||this.aborted<0||this.passed>this.total||this.skipped>this.total||this.failed>this.total){
-            grade= "InvalidGrade";
-            return grade;
-        }
-
+        try {
+            if(this.total<0) throw "total testCases are not less than 0";
+            if(this.passed<0) throw "passed test cases are not less than 0";
+            if(this.failed<0) throw "failed test cases are not less than 0";
+            if(this.skipped<0) throw "skipped test cases are not less than 0";
+            if(this.aborted<0) throw "aborted test cases are not less than 0";
+            if(this.passed>this.total) throw "passed test cases are not greater than to  total test cases";
+            if(this.skipped>this.total) throw "total skipped test cases are not greater than total  test cases";
+            if(this.failed>this.total) throw "total failed test cases are not greater than total test cases";
+            }
+        catch(err) {
+              
+           return err;
+          }
         let percentage= this.passed/(this.total- (this.skipped+this.aborted));
        
         percentage=parseInt(percentage*100);
@@ -48,16 +56,13 @@ class gradeSystem{
             }
         }
         
-    Object.keys(grades).forEach(function(key){
+      Object.keys(grades).forEach(function(key){
         var value = grades[key];
         if(percentage < value['upper'] && percentage > value['lower'])
             grade=key;
             
-    });
- 
-
-        
-    //  return {grade,percentage};
+        });
+    
      return grade;
 
     }
@@ -65,14 +70,10 @@ class gradeSystem{
 
 }
 
-
 module.exports={
     
     gradeSystem
     
   }
-//   "scripts": {
-//     "start": "node index.js",
-//     "test": "nyc mocha test/**/*.spec.js"
-//   },
+
 
